3e192fca00c00b3d4ebb423d944a47935d67da87
Treesa Jose <2treesajo@gmail.com>
2026-05-11 10:36:00 +0530

    Time: 12 ms (39.49%), Space: 15.6 MB (91.32%) - LeetHub

--- 0155-min-stack/0155-min-stack.py
+++ 0155-min-stack/0155-min-stack.py
@@ -1,6 +1,7 @@
 class MinStack(object):
 
     def __init__(self):
+        self.s=[]
         self.st=[]
 
     def push(self, val):
@@ -8,31 +9,39 @@ class MinStack(object):
         :type val: int
         :rtype: None
         """
-        self.st.append(val)
-
-        
+        self.s.append(val)
+        # if min stk is empty or val is less than -1 pos in min stk
+        if not self.st or (val<= self.st[-1]):
+            self.st.append(val)
 
     def pop(self):
         """
         :rtype: None
         """
-        self.st.pop()
+        # if element to be popped from s is smallest ie; in st -1 pos 
+        # then pop it too.
+        if self.s.pop() == self.st[-1]:
+            self.st.pop()
 
     def top(self):
         """
         :rtype: int
         """
-        return self.st[-1]
-        
+        #return -1 if not self.s else self.s[-1]
+        if self.s:
+            return self.s[-1]
+        else:
+            return -1
 
     def getMin(self):
         """
         :rtype: int
         """
-        a=min(self.st)
-
-        return a 
-        
+        #return -1 if not self.st else self.st[-1]
+        if self.st:
+            return self.st[-1]
+        else:
+            return -1
 
 
 # Your MinStack object will be instantiated and called as such:
