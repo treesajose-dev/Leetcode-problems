8120b565ed7a1550f0c671d00f7c7884ff5f5ee8
Treesa Jose <2treesajo@gmail.com>
2026-05-11 15:07:55 +0530

    Time: 7 ms (47.01%), Space: 12.9 MB (6.52%) - LeetHub

--- 1021-remove-outermost-parentheses/1021-remove-outermost-parentheses.py
+++ 1021-remove-outermost-parentheses/1021-remove-outermost-parentheses.py
@@ -6,24 +6,21 @@ class Solution(object):
         """
         ans = ""
         count = 0
+        i=0
+        j=0
 
-        for ch in s:
-
-            if ch == '(':
-
-                if count > 0:
-                    ans += ch
-
-                count += 1
-
-            else:
-
-                count -= 1
-
-                if count > 0:
-                    ans += ch
-
+        for j in range(len(s)):
+            if s[j]=='(':
+                count+=1
+            elif s[j]==')':
+                count-=1
+            
+            if count==0:
+                ans+=s[i+1:j]
+                i=j+1
+        
         return ans
+        
             
             
 
