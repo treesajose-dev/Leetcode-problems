234f2af040dc65a7252aec6317447bc229189170
Treesa Jose <2treesajo@gmail.com>
2026-05-13 14:39:03 +0530

    Time: 0 ms (100%), Space: 12.3 MB (94.53%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.py
+++ 0118-pascals-triangle/0118-pascals-triangle.py
@@ -4,24 +4,12 @@ class Solution(object):
         :type numRows: int
         :rtype: List[List[int]]
         """
-        dp=[]
-        dp.append([1])
-        if numRows==1:
-            return dp
+        dp=[0]*numRows
 
-        dp.append([1,1])
-
-        if numRows==2:
-            return dp
-
-        for i in range(3, numRows + 1):
-            prev = dp[i-2]
-            temp = [1]
-
-            for j in range(len(prev)-1):
-                temp.append(prev[j] + prev[j+1])
-
-            temp.append(1)
-            dp.append(temp)
-
+        for i in range(numRows):
+            dp[i]=[0]*(i+1)
+            dp[i][0]=1
+            dp[i][i]=1
+            for j in range(1,i):
+                dp[i][j]=dp[i-1][j-1]+dp[i-1][j]
         return dp
