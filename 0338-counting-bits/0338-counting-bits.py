590d1cceaf9fc3979d554c276814dd798e43945f
Treesa Jose <2treesajo@gmail.com>
2026-05-13 12:13:49 +0530

    Time: 7 ms (75.08%), Space: 17.8 MB (18.39%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.py
+++ 0338-counting-bits/0338-counting-bits.py
@@ -5,17 +5,16 @@ class Solution(object):
         :rtype: List[int]
         """
 
+        # odd numbers right shift one 1 is gone
+        # even numbers right shift no. of ones remains same
+
+        # odd nos & 1 gives 1
+        #even nos & 1 gives 0
+
         ans=[0]*(n+1)
 
-        
-        for i in range(n+1):
-            temp=i
-            co=0
-            while temp:
-                temp=temp&(temp-1)
-                co+=1
-            ans[i]=co
+        for i in range(1,n+1):
+            ans[i]=ans[i>>1] + (i & 1)
 
         return ans
-
         
