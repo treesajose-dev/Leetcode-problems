e57f9d9438d0260e49f94f9abfe193d4ec68b30a
Treesa Jose <2treesajo@gmail.com>
2026-05-13 09:57:21 +0530

    Time: 18 ms (58.88%), Space: 12.3 MB (53.25%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.py
+++ 0509-fibonacci-number/0509-fibonacci-number.py
@@ -1,18 +1,18 @@
 class Solution(object):
     def fib(self, n):
-        pn = 0   # previous number F(0)
-        cn = 1   # current number F(1)
+        # initialize list with 0th term in fibonacci series
+        dp=[0]
 
-        # base cases
-        if n == 0:
-            return pn
-        elif n == 1:
-            return cn
+        if n==0:
+            return dp[0]
+        dp.append(1)
 
-        # calculate from F(2) to F(n)
-        for i in range(2, n + 1):
-            nn = pn + cn
-            pn = cn
-            cn = nn
+        if n==1:
+            return dp[1]
+        
+        for i in range(2,n+1):
+            dp.append(dp[-1]+dp[-2])
 
-        return cn
+        return dp[-1]
+
+
