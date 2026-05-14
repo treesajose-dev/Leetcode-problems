8ffbb0e0aa1c49853d4275ed357e48984a129f6e
Treesa Jose <2treesajo@gmail.com>
2026-05-14 12:35:47 +0530

    Time: 35 ms (5.58%), Space: 13.1 MB (43.65%) - LeetHub

--- 0414-third-maximum-number/0414-third-maximum-number.py
+++ 0414-third-maximum-number/0414-third-maximum-number.py
@@ -9,10 +9,7 @@ class Solution(object):
 
         lis=list(s)
 
-        for i in range(0,len(lis)):
-            for j in range(0,len(lis)-i-1):
-                if lis[j]>lis[j+1]:
-                    lis[j],lis[j+1]=lis[j+1],lis[j]
+        lis.sort()
 
         print(lis)
 
