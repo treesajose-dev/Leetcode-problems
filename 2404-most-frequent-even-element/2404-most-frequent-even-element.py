06ed600644ba23dc79533d56f77a6dde50939dbe
Treesa Jose <2treesajo@gmail.com>
2026-06-19 23:10:32 +0530

    Time: 17 ms (93.27%), Space: 12.8 MB (31.39%) - LeetHub

--- 2404-most-frequent-even-element/2404-most-frequent-even-element.py
+++ 2404-most-frequent-even-element/2404-most-frequent-even-element.py
@@ -4,18 +4,23 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        ans = -1
-        max_freq = 0
+        freq = {}
 
         for x in nums:
             if x % 2 == 0:
-                freq = nums.count(x)
+                freq[x] = freq.get(x, 0) + 1
 
-                if freq > max_freq:
-                    max_freq = freq
-                    ans = x
-                elif freq == max_freq:
-                    if ans == -1 or x < ans:
-                        ans = x
+        if not freq:
+            return -1
+
+        ans = -1
+        max_freq = 0
+
+        for num, count in freq.items():
+            if count > max_freq:
+                max_freq = count
+                ans = num
+            elif count == max_freq and num < ans:
+                ans = num
 
         return ans
